ccd0951094d1f8aaba16206f2af583da5b2ffe3b
jgrove90 <jgrove90@gmail.com>
2023-05-29 14:14:17 -0600

    formatted with black

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,21 @@
 import logging
-from importlib.metadata import version 
+from importlib.metadata import version
 
 # set log file name for app
 LOG_FILE_NAME = "app.log"
+
 
 def get_package_version(package_name: str) -> str:
     """Retreives the version of a python package"""
     return version(package_name)
 
-def setup_logger(name:str, log_file:str) -> logging.Logger:
+
+def setup_logger(name: str, log_file: str) -> logging.Logger:
     """Defines the logging setup"""
 
-    formatter = logging.Formatter('%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
+    )
 
     # create console and file handlers
     console_handler = logging.StreamHandler()
@@ -19,7 +23,6 @@ def setup_logger(name:str, log_file:str) -> logging.Logger:
 
     file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(formatter)
-
 
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
