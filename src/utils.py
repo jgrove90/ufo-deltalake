90df2f8821c14f1a5ca329cf5b28dfb9dd99a9e7
jgrove90 <jgrove90@gmail.com>
2023-06-01 09:23:43 -0600

    added soup_html() function

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import logging
+import requests
 from importlib.metadata import version
+from bs4 import BeautifulSoup
 
 # set log file name for app
 LOG_FILE_NAME = "app.log"
@@ -10,6 +12,7 @@ def get_package_version(package_name: str) -> str:
     return version(package_name)
 
 
+# TODO add attribute to either print to console, file, or both
 def setup_logger(name: str, log_file: str) -> logging.Logger:
     """Defines the logging setup"""
 
@@ -30,3 +33,20 @@ def setup_logger(name: str, log_file: str) -> logging.Logger:
     logger.addHandler(console_handler)
 
     return logger
+
+
+# TODO Create test
+def soup_html(url: str) -> BeautifulSoup:
+    """Returns BeautifulSoup html object ready for parsing."""
+    try:
+        logger = setup_logger("webscraping", LOG_FILE_NAME)
+
+        # retreive html and create soup object for parsing
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        logger.info(f"Received response from {url}")
+
+        return soup
+    except requests.exceptions.RequestException as e:
+        logger.error(f"{e}")
