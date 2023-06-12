d565f9f2f409e2e00fa6a812d8a3912881d0c7e6
jgrove90 <jgrove90@gmail.com>
2023-06-12 12:37:23 -0600

    added more tables

--- src/spark/tables.py
+++ src/spark/tables.py
@@ -50,20 +50,147 @@ def create_ufo_silver_table(spark: SparkSession, table_name: str) -> DeltaTable:
                 DeltaTable.createIfNotExists(spark)
                 .tableName(table_name)
                 .addColumn("id", "LONG")
+                .addColumn("id_location", "LONG")
                 .addColumn("city", "STRING")
                 .addColumn("state", "STRING")
                 .addColumn("country", "STRING")
+                .addColumn("id_description", "LONG")
                 .addColumn("shape", "STRING")
                 .addColumn("duration", "STRING")
                 .addColumn("summary", "STRING")
                 .addColumn("images", "STRING")
+                .addColumn("id_date", "LONG")
                 .addColumn("date", "DATE")
                 .addColumn("year", "INT")
                 .addColumn("month", "INT")
                 .addColumn("dayofweek", "STRING")
                 .addColumn("week", "INT")
                 .addColumn("hour", "INT")
+                .addColumn("id_astro", "LONG")
                 .addColumn("moonPhaseAngle", "DOUBLE")
+                .location(f".{path}")
+                .execute()
+            )
+
+            logger.info(f"Created delta table: {table_name} located @ '{path}'")
+
+            return table
+    except Exception as e:
+        logger.error(f"{e}")
+
+def create_ufo_gold_dim_location(spark: SparkSession, table_name: str) -> DeltaTable:
+    """Creates dimension location"""
+    try:
+        path = "./lakehouse/ufo/gold/dim_location"
+
+        if DeltaTable.isDeltaTable(spark, f"{path}"):
+            logger.info(f"Delta table: '{table_name}' already exists @ '{path}'")
+        else:
+            table = (
+                DeltaTable.createIfNotExists(spark)
+                .tableName(table_name)
+                .addColumn("id_location", "LONG")
+                .addColumn("city", "STRING")
+                .addColumn("state", "STRING")
+                .addColumn("country", "STRING")
+                .location(f".{path}")
+                .execute()
+            )
+
+            logger.info(f"Created delta table: {table_name} located @ '{path}'")
+
+            return table
+    except Exception as e:
+        logger.error(f"{e}")
+
+def create_ufo_gold_dim_description(spark: SparkSession, table_name: str) -> DeltaTable:
+    """Creates dimension description"""
+    try:
+        path = "./lakehouse/ufo/gold/dim_description"
+
+        if DeltaTable.isDeltaTable(spark, f"{path}"):
+            logger.info(f"Delta table: '{table_name}' already exists @ '{path}'")
+        else:
+            table = (
+                DeltaTable.createIfNotExists(spark)
+                .addColumn("id_description", "LONG")
+                .addColumn("shape", "STRING")
+                .addColumn("duration", "STRING")
+                .addColumn("summary", "STRING")
+                .addColumn("images", "STRING")
+                .location(f".{path}")
+                .execute()
+            )
+
+            logger.info(f"Created delta table: {table_name} located @ '{path}'")
+
+            return table
+    except Exception as e:
+        logger.error(f"{e}")
+
+def create_ufo_gold_dim_date(spark: SparkSession, table_name: str) -> DeltaTable:
+    """Creates dimension date"""
+    try:
+        path = "./lakehouse/ufo/gold/dim_date"
+
+        if DeltaTable.isDeltaTable(spark, f"{path}"):
+            logger.info(f"Delta table: '{table_name}' already exists @ '{path}'")
+        else:
+            table = (
+                DeltaTable.createIfNotExists(spark)
+                .addColumn("id_date", "LONG")
+                .addColumn("date", "DATE")
+                .addColumn("year", "INT")
+                .addColumn("month", "INT")
+                .addColumn("dayofweek", "STRING")
+                .addColumn("week", "INT")
+                .addColumn("hour", "INT")
+                .location(f".{path}")
+                .execute()
+            )
+
+            logger.info(f"Created delta table: {table_name} located @ '{path}'")
+
+            return table
+    except Exception as e:
+        logger.error(f"{e}")
+
+def create_ufo_gold_dim_astro(spark: SparkSession, table_name: str) -> DeltaTable:
+    """Creates dimension astro"""
+    try:
+        path = "./lakehouse/ufo/gold/dim_astro"
+
+        if DeltaTable.isDeltaTable(spark, f"{path}"):
+            logger.info(f"Delta table: '{table_name}' already exists @ '{path}'")
+        else:
+            table = (
+                DeltaTable.createIfNotExists(spark)
+                .addColumn("id_astro", "LONG")
+                .addColumn("moonPhaseAngle", "DOUBLE")
+                .location(f".{path}")
+                .execute()
+            )
+
+            logger.info(f"Created delta table: {table_name} located @ '{path}'")
+
+            return table
+    except Exception as e:
+        logger.error(f"{e}")
+
+def create_ufo_gold_fact(spark: SparkSession, table_name: str) -> DeltaTable:
+    """Creates dimension fact"""
+    try:
+        path = "./lakehouse/ufo/gold/fact"
+
+        if DeltaTable.isDeltaTable(spark, f"{path}"):
+            logger.info(f"Delta table: '{table_name}' already exists @ '{path}'")
+        else:
+            table = (
+                DeltaTable.createIfNotExists(spark)
+                .addColumn("id_location", "LONG")
+                .addColumn("id_description", "LONG")
+                .addColumn("id_date", "LONG")
+                .addColumn("id_astro", "LONG")
                 .location(f".{path}")
                 .execute()
             )
